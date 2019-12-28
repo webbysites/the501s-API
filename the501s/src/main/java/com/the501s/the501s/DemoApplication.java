5233b49319ebadf19bde6cf748f04af56a403a04
Travis Vela <jtravisvela@gmail.com>
2019-12-27 19:07:21 -0600

    add basic spring boot app

---
+++ the501s/src/main/java/com/the501s/the501s/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.the501s.the501s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
