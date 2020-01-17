8bb69d632322526d9eb0d515d341d1127105529e
Travis Vela <jtravisvela@gmail.com>
2020-01-16 22:31:18 -0600

    update production environment

--- the501s/src/main/java/com/the501s/the501s/DemoApplication.java
+++ the501s/src/main/java/com/the501s/the501s/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -22,4 +24,14 @@ public class DemoApplication {
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+          registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*");
+        }
+      };
+    }
 }
